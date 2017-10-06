4d7d4f84e4df88042b52caa51b6241c26997ff5f
Ryan Crispin Heneise <ryan@mysmallidea.com>
2017-10-05 23:47:33 -0500

    Added blog.

--- Gemfile
+++ Gemfile
@@ -1,11 +1,18 @@
 source 'http://rubygems.org'
 
 gem 'middleman', '~> 4'
+gem 'middleman-blog'
 gem 'middleman-livereload', '~> 3'
-gem 'middleman-minify-html',   '~> 3.4.1'
-gem 'middleman-sitemap', git: 'https://github.com/matsu911/middleman-sitemap', branch: 'master'
+gem 'middleman-minify-html', '~> 3.4.1'
 gem 'middleman-robots'
+gem 'middleman-sitemap', git: 'https://github.com/matsu911/middleman-sitemap', branch: 'master'
 gem 'middleman-sitemap-ping'
+
+gem 'redcarpet', '~> 3.3', '>= 3.3.3'
+gem 'nokogiri'
+
+# For feed.xml.builder
+gem 'builder', '~> 3.0'
 
 gem 'hamlit'
 gem 'rake'
--- config.rb
+++ config.rb
@@ -1,8 +1,40 @@
-# encoding: utf-8
-
 require 'hamlit'
 
-page '/index.html'
+# Per-page layout changes:
+#
+# With no layout
+page '/*.xml', layout: false
+page '/*.json', layout: false
+page '/*.txt', layout: false
+
+activate :blog do |blog|
+  blog.name = 'articles'
+  # This will add a prefix to all links, template references and source paths
+  # blog.prefix = "articles"
+
+  # blog.permalink = "{year}/{month}/{day}/{title}.html"
+  blog.permalink = "articles/{title}"
+  # Matcher for blog source files
+  # blog.sources = "{year}-{month}-{day}-{title}.html"
+  blog.taglink = "articles/tags/{tag}.html"
+  blog.layout = "article"
+  # blog.summary_separator = /(READMORE)/
+  # blog.summary_length = 250
+  blog.year_link = "articles/{year}.html"
+  blog.month_link = "articles/{year}/{month}.html"
+  blog.day_link = "articles/{year}/{month}/{day}.html"
+  # blog.default_extension = ".markdown"
+
+  blog.tag_template = "tag.html"
+  blog.calendar_template = "calendar.html"
+
+  # Enable pagination
+  # blog.paginate = true
+  # blog.per_page = 10
+  # blog.page_link = "page/{num}"
+end
+
+page "/feed.xml", layout: false
 
 #
 # Use webpack for assets
