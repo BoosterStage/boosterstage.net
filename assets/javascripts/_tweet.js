497b6f90e99aca10fe4a618a35c26b6e110d3bc6
Ryan Crispin Heneise <ryan@mysmallidea.com>
2017-11-15 20:09:17 -0600

    Added article.

--- assets/javascripts/_tweet.js
+++ assets/javascripts/_tweet.js
@@ -1,14 +1,19 @@
 !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
 
-var elements = document.querySelectorAll('blockquote.tweet-this');
+document.addEventListener('DOMContentLoaded', function(){
 
-elements.forEach((el) => {
-  var blockQuoteText = el.textContent
-  var currentUrl = window.location;
-  var tweetStr = document.createElement('div')
-  tweetStr.classList.add('tweet-quote')
-  tweetStr.innerHTML = '<a href="https://twitter.com/share" data-text="' +blockQuoteText+ '"  class="twitter-share-button" data-via="Booster_Stage" data-url="' +currentUrl+ '" data-size="large" data-count="vertical">Tweet</a>'
-  el.appendChild(tweetStr);
+  var elements = document.querySelectorAll('blockquote.tweet-this');
+
+  elements.forEach((el) => {
+    var blockQuoteText = el.textContent;
+    var currentUrl = window.location;
+    var tweetStr = document.createElement('div')
+
+    tweetStr.classList.add('tweet-quote')
+    tweetStr.innerHTML = '<a href="https://twitter.com/share" data-text="' +blockQuoteText+ '"  class="twitter-share-button" data-via="Booster_Stage" data-url="' +currentUrl+ '" data-size="large" data-count="vertical">Tweet</a>'
+
+    el.appendChild(tweetStr);
+  })
 
 })
 
