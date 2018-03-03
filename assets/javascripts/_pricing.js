30b610e7e2b0144d73ebd7f41f66a22c0ed5c6c2
Ryan Crispin Heneise <ryan@boosterstage.net>
2018-03-02 21:37:17 -0600

    Moved Drip scripts to end of file.

--- assets/javascripts/_pricing.js
+++ assets/javascripts/_pricing.js
@@ -2,7 +2,7 @@ import Cookies from 'js-cookie';
 
 document.addEventListener('DOMContentLoaded', function(){
   var pricingNav = document.querySelectorAll('[data-pricing]')
-  if (pricingNav && !Cookies.get('viewed_pricing_guide') ) {
+  if (pricingNav && !Cookies.get('viewed_pricing_guide') && typeof(_dcq) != 'undefined' ) {
     pricingNav.forEach((el) => {
       el.addEventListener('click', function(e){
         e.preventDefault()
